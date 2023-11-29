680057e95a5ff695db32a41d74df0cd445d8d7b0
salman3009 <salman444g@gmail.com>
2023-11-29 21:21:11 +0530

    Code

---
+++ Course/November/29-11-23/basic-react/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function Employee(){
+     
+    return <div id="heading" className="headingDetails">
+         <input type="text"/>
+         <img/>
+         <br/>
+         <h1>hello world - salman</h1>
+         <h2>Welcome to CSS</h2>
+         <p>Welcome to javascript</p>
+          </div>
+  
+}
+
+export default Employee;
+//Employee component name
---
+++ Course/November/29-11-23/basic-react/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Employee from './App';
+//Employee is the component name 
+//App is the file name
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Employee/>
+);
+
+
