f8106d3a4f0ebca2ac1746f30369199d26ae6fff
salman3009 <salman444g@gmail.com>
2023-12-01 22:59:46 +0530

    code

--- Course/December/01-12-23/image-map/src/App.js
+++ Course/December/01-12-23/image-map/src/App.js
@@ -1,13 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function App() {
-  
+  const employee = [
+    {
+      id: "1",
+      name: "rajesh",
+      email: "rajesh@gmail.com",
+      status: true
+    },
+    {
+      id: "1",
+      name: "kumar",
+      email: "rak@gmail.com",
+      status: false
+    },
+    {
+      id: "1",
+      name: "suresh",
+      email: "sur@gmail.com",
+      status: true
+    }
+  ];
 
   return (
     <div className="App">
-         hello world
+       {employee.map((input,index)=>{
+            return (<div key={index}>
+              <h1>{input.email}</h1>
+              <h1>{input.name}</h1>
+            </div>)
+       })
+       }
     </div>
   );
 }
