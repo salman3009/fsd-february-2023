25c1dea94724ec4238952d890336cadcbc3a5a58
salman3009 <salman444g@gmail.com>
2023-12-01 22:01:29 +0530

    code

--- Course/December/01-12-23/image-map/src/App.js
+++ Course/December/01-12-23/image-map/src/App.js
@@ -7,7 +7,6 @@ function App() {
   const [getIndex,setIndex] = useState(0);
 
   const NextPageHandler=()=>{
-    debugger;
     if(getIndex>=(image.length-1)){
       setIndex(0);
     }else{
@@ -15,11 +14,21 @@ function App() {
     }  
   }
 
+  const previousPageHandler=()=>{
+    //4<=4
+    if(getIndex<=0){
+      setIndex(0);
+    }else{
+      setIndex(getIndex-1);
+    }  
+  }
+  
+
   return (
     <div className="App">
          <div>{undefined}{false}{null}{''}</div>
          <div>{image[getIndex]}</div>
-         <button>Previous</button>
+         <button onClick={previousPageHandler}>Previous</button>
          <button onClick={NextPageHandler}>Next</button>
     </div>
   );
