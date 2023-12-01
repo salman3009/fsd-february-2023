49ba7d8d8a55562013300912eb412a056ad4241d
salman3009 <salman444g@gmail.com>
2023-12-01 21:45:58 +0530

    code

---
+++ Course/December/01-12-23/image-map/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+function App() {
+  const image=[11,22,33,44,55];
+  const [getIndex,setIndex] = useState(0);
+
+  const NextPageHandler=()=>{
+    debugger;
+    if(getIndex>=(image.length-1)){
+      setIndex(0);
+    }else{
+      setIndex(getIndex+1);
+    }  
+  }
+
+  return (
+    <div className="App">
+         <div>{undefined}{false}{null}{''}</div>
+         <div>{image[getIndex]}</div>
+         <button>Previous</button>
+         <button onClick={NextPageHandler}>Next</button>
+    </div>
+  );
+}
+
+export default App;
