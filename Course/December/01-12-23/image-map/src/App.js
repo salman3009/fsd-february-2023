d7d923f20246dc5af02e0ef8f1a2422b9d9d1d82
salman3009 <salman444g@gmail.com>
2023-12-01 22:42:50 +0530

    code

--- Course/December/01-12-23/image-map/src/App.js
+++ Course/December/01-12-23/image-map/src/App.js
@@ -3,32 +3,11 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-  const image=[11,22,33,44,55];
-  const [getIndex,setIndex] = useState(0);
-
-  const NextPageHandler=()=>{
-    if(getIndex>=(image.length-1)){
-      setIndex(0);
-    }else{
-      setIndex(getIndex+1);
-    }  
-  }
-
-  const previousPageHandler=()=>{
-    if(getIndex<=0){
-      setIndex(image.length-1);
-    }else{
-      setIndex(getIndex-1);
-    }  
-  }
   
 
   return (
     <div className="App">
-         <div>{undefined}{false}{null}{''}</div>
-         <div>{image[getIndex]}</div>
-         <button onClick={previousPageHandler}>Previous</button>
-         <button onClick={NextPageHandler}>Next</button>
+         hello world
     </div>
   );
 }
