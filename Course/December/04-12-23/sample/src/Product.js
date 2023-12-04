352797f47f33b13369f6569ba545768462be028a
salman3009 <salman444g@gmail.com>
2023-12-04 21:49:27 +0530

    code

--- Course/December/04-12-23/sample/src/Product.js
+++ Course/December/04-12-23/sample/src/Product.js
@@ -3,9 +3,13 @@ import './Product.css';
 function Product(props){
     //destructuring of object
     const {name,price} = props;
+
+    let styleObject = {color:"red",marginTop:"40px"};
+    
     return (<div className="box">
-        <h1 id={name} style={{color:"red"}}>Product Name:{name}</h1>
-        <h1 style={{color:"blue"}}>Product Price:{price}</h1>
+        <h1 id={name} style={styleObject}>Product Name:{name}</h1>
+        <h1 style={{color:"blue",backgroundColor:"grey"}}>Product Price:{price}</h1>
+        <h3>{name ==="sony"?"40% discount":"20% discount"}</h3>
     </div>)
 }
 
