eecbdbccacaf0fbd739dd251bef59b8a775c97c6
salman3009 <salman444g@gmail.com>
2023-12-04 21:14:54 +0530

    code

--- Course/December/04-12-23/sample/src/Product.js
+++ Course/December/04-12-23/sample/src/Product.js
@@ -1,9 +1,9 @@
-
+import './Product.css';
 
 function Product(props){
     //destructuring of object
     const {name,price} = props;
-    return (<div>
+    return (<div className="box">
         <h1>Product Name:{name}</h1>
         <h1>Product Price:{price}</h1>
     </div>)
