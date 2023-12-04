7e547234c056062dfa22d3df27aad0d44c3a5b14
salman3009 <salman444g@gmail.com>
2023-12-04 21:09:12 +0530

    code

--- Course/December/04-12-23/sample/src/Product.js
+++ Course/December/04-12-23/sample/src/Product.js
@@ -1,9 +1,11 @@
 
 
 function Product(props){
+    //destructuring of object
+    const {name,price} = props;
     return (<div>
-        <h1>Product Name:{props.name}</h1>
-        <h1>Product Price:{props.price}</h1>
+        <h1>Product Name:{name}</h1>
+        <h1>Product Price:{price}</h1>
     </div>)
 }
 
