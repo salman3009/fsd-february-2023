44e1f3476b01486507bf3d811c58f001b47f1291
salman3009 <salman444g@gmail.com>
2023-12-04 23:05:49 +0530

    code

--- Course/December/04-12-23/sample/src/Product.js
+++ Course/December/04-12-23/sample/src/Product.js
@@ -1,10 +1,26 @@
 import './Product.css';
+import {useState,useEffect} from 'react';
 
 function Product(){
-   
+
+     const[getPage,setPage] = useState(0);
+
+     useEffect(()=>{
+        alert("callback");
+     },[getPage])
+
+     useEffect(()=>{
+        alert("intial rendering");
+     },[])
+
+     const onChangeHandler=()=>{
+        setPage(getPage+1);//asynchronous
+        
+     }
 
     return (<div className="box">
-         <h1>hello</h1>
+         <h1>{getPage}</h1>
+         <button onClick={onChangeHandler}>Click</button>
     </div>)
 }
 
