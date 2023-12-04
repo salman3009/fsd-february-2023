3bb62be04b1cfb5dd4d1fd108c0c96d9b79d6c90
salman3009 <salman444g@gmail.com>
2023-12-04 23:23:46 +0530

    code

--- Course/December/04-12-23/sample/src/Product.js
+++ Course/December/04-12-23/sample/src/Product.js
@@ -4,6 +4,7 @@ import {useState,useEffect} from 'react';
 function Product(){
 
      const[getPage,setPage] = useState(1);
+     const[getData,setData] = useState([]);
 
      useEffect(()=>{
         intial();
@@ -23,6 +24,7 @@ function Product(){
            let response = await fetch(`https://content.newtonschool.co/v1/pr/63b6c911af4f30335b4b3b89/products?page=${getPage}&limit=5`);
            let data = await response.json();
            console.log(data);
+           setData(data);
         }catch(err){
            console.log(err);
         }
@@ -32,6 +34,11 @@ function Product(){
     return (<div className="box">
          <h1>{getPage}</h1>
          <button onClick={onChangeHandler}>Click</button>
+         {getData.map((data)=>{
+              return(<div>
+                <img src={data.image}/>
+              </div>)
+         })}
     </div>)
 }
 
