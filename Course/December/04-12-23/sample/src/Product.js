969ad4496ec333dc90c8430b1c29fa7379ab5666
salman3009 <salman444g@gmail.com>
2023-12-04 23:12:01 +0530

    code

--- Course/December/04-12-23/sample/src/Product.js
+++ Course/December/04-12-23/sample/src/Product.js
@@ -3,14 +3,14 @@ import {useState,useEffect} from 'react';
 
 function Product(){
 
-     const[getPage,setPage] = useState(0);
+     const[getPage,setPage] = useState(1);
 
      useEffect(()=>{
         alert("callback");
      },[getPage])
 
      useEffect(()=>{
-        alert("intial rendering");
+        intial();
      },[])
 
      const onChangeHandler=()=>{
@@ -18,6 +18,17 @@ function Product(){
         
      }
 
+     const intial = async()=>{
+        try{
+           let response = await fetch(`https://content.newtonschool.co/v1/pr/63b6c911af4f30335b4b3b89/products?page=${getPage}&limit=5`);
+           let data = await response.json();
+           console.log(data);
+        }catch(err){
+           console.log(err);
+        }
+     }
+
+     
     return (<div className="box">
          <h1>{getPage}</h1>
          <button onClick={onChangeHandler}>Click</button>
