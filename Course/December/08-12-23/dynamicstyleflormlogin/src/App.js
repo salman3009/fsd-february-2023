8eab0699e70ce46dc8a03d3fc4ab1e3e7524dbb4
salman3009 <salman444g@gmail.com>
2023-12-08 21:36:40 +0530

    code

---
+++ Course/December/08-12-23/dynamicstyleflormlogin/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://shorturl.at/dkBCO" style={{width:"200px",height:"200px"}}/>
+    </div>
+  );
+}
+
+export default App;
