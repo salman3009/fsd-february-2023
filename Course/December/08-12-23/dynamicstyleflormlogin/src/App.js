ec0d0137fe87d9b8e20d6ddf4fd3c23b9a728396
salman3009 <salman444g@gmail.com>
2023-12-08 21:46:00 +0530

    code

--- Course/December/08-12-23/dynamicstyleflormlogin/src/App.js
+++ Course/December/08-12-23/dynamicstyleflormlogin/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const[getWidth,setWidth] = useState(200);
+
+  const setWidthIncreaseHandler=()=>{
+    setWidth(getWidth+50);
+  }
+
+  const setWidthDecreaseHandler=()=>{
+    setWidth(getWidth-50);
+  }
+
   return (
     <div className="App">
-      <img src="https://shorturl.at/dkBCO" style={{width:"200px",height:"200px"}}/>
+      <img src="https://shorturl.at/dkBCO" style={{width:`${getWidth}px`,height:"200px"}}/>
+      <br/>
+      <button onClick={setWidthIncreaseHandler}>Increase width</button>
+      <button onClick={setWidthDecreaseHandler}>Decrease width</button>
     </div>
   );
 }
