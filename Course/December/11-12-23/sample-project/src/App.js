c48325251bbca1edb3d14c9538101893ae99cd0c
salman3009 <salman444g@gmail.com>
2023-12-11 22:37:32 +0530

    code

--- Course/December/11-12-23/sample-project/src/App.js
+++ Course/December/11-12-23/sample-project/src/App.js
@@ -5,7 +5,6 @@ import {useEffect,useState} from 'react';
 function App() {
 
   const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  console.log(keys);
   //part 1
  const[getQuote,setQuote] = useState('');
  const [getInput,setInput] = useState('');
@@ -30,10 +29,8 @@ function App() {
         }
   }
 
-  const onChangeHandler=(key)=>{
-     alert(key);
-     setInput(key);//asynchronous code
-  
+  const onChangeHandler=(event)=>{
+     setInput(getInput+event.target.value);
     }
 
     if(getQuote){
@@ -42,13 +39,14 @@ function App() {
 
   //part 2 //part 5
   return (
+   
     <div className="App">
-        <button onClick={()=>onChangeHandler("details")}>Details</button>
-        {/* <input type="text" onChange={onChangeHandler}/> */}
+       {getInput}
+        <input type="text" onChange={onChangeHandler}/>
         {
           keys.map((input)=>{
                return <span key={input}>
-                 {input ==' '? <button onClick={()=>onChangeHandler(input)}>space</button>:<button onClick={()=>onChangeHandler(input)}>{input}</button>}
+                 {input ==' '? <button value={input} onClick={onChangeHandler}>space</button>:<button value={input} onClick={onChangeHandler}>{input}</button>}
                </span>
           })
         }
