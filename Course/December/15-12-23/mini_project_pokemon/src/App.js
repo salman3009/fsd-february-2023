cc81b1a33ecf7874ab6e8b6cd0eab47af7de8ff9
salman3009 <salman444g@gmail.com>
2023-12-15 21:00:32 +0530

    code

--- Course/December/15-12-23/mini_project_pokemon/src/App.js
+++ Course/December/15-12-23/mini_project_pokemon/src/App.js
@@ -2,6 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+   const getAllDetails= async ()=>{
+      try{
+        let response = await fetch("https://content.newtonschool.co/v1/pr/64ccef982071a9ad01d36ff6/pokemonspages1");
+        let data = await response.json();
+        console.log(data);
+      }catch(err){
+        console.log(err);
+      }
+   }
+
+
   return (
     <div className="App">
      <h1>Hello world</h1>
