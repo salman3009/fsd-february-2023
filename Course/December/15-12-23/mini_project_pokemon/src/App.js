d29bc6978112a9771d33ed4fc2290771ab76ac56
salman3009 <salman444g@gmail.com>
2023-12-15 21:19:19 +0530

    code

--- Course/December/15-12-23/mini_project_pokemon/src/App.js
+++ Course/December/15-12-23/mini_project_pokemon/src/App.js
@@ -1,26 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 
 function App() {
 
-   const getAllDetails= async ()=>{
-      try{
-        let response = await fetch("https://content.newtonschool.co/v1/pr/64ccef982071a9ad01d36ff6/pokemonspages1");
-        let data = await response.json();
-        console.log(data);
-      }catch(err){
-        console.log(err);
-      }
-   }
+  const getAllDetails = async () => {
+    try {
+      let response = await fetch("https://content.newtonschool.co/v1/pr/64ccef982071a9ad01d36ff6/pokemonspages1");
+      let data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
-   useEffect(()=>{
-        alert("initial load");
-   },[])
+  useEffect(() => {
+    getAllDetails();
+  }, [])
 
   return (
     <div className="App">
-     <h1>Hello world</h1>
+      <h1>Hello world</h1>
     </div>
   );
 }
