103bec0567c8be4929428619b95cdadd8b3f4e6b
salman3009 <salman444g@gmail.com>
2023-12-18 23:19:21 +0530

    code

--- Course/December/18-12-23_pokemon_project/pokemon-project/src/App.js
+++ Course/December/18-12-23_pokemon_project/pokemon-project/src/App.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 function App() {
 
   const [getList, setList] = useState([]);
-  const[getAPI,setAPI] = useState('https://content.newtonschool.co/v1/pr/64ccef982071a9ad01d36ff6/pokemonspages1');
+  const [getAPI, setAPI] = useState('https://content.newtonschool.co/v1/pr/64ccef982071a9ad01d36ff6/pokemonspages1');
 
   const getAllPokemon = async () => {
     try {
@@ -29,7 +29,7 @@ function App() {
     getAllPokemon();
   }, [])
 
-  const nextLoadData = ()=>{
+  const nextLoadData = () => {
     getAllPokemon();
   }
 
@@ -41,11 +41,11 @@ function App() {
             {getList.map((obj, index) => {
               return (<div key={index} className={`thumb-container ${obj.type}`}>
                 <div className="number">
-                  <small>{obj.id}</small>
+                  <small>#{obj.id}</small>
                 </div>
                 <img src={obj.image} alt={obj.name} />
                 <div className="detail-wrapper">
-                  <h3>{obj.name}</h3>
+                  <h3>{obj.name.toUpperCase()}</h3>
                   <small>Type: {obj.type}</small>
                   <button className="pokeinfo">Know more...</button>
                 </div>
@@ -53,6 +53,97 @@ function App() {
             })}
           </div>
           <button className="load-more" onClick={nextLoadData}>More Pokemons</button>
+        </div>
+      </div>
+
+
+      <div class="expanded-overlay normal visible"><button class="close-button normal ">X</button>
+        <div class="expanded-left"><img class="expanded-image"
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/19.svg"
+          alt="rattata" />
+          <h3 class="expanded-name">rattata</h3>
+        </div>
+        <div class="expanded-right  normal ">
+          <table class="expanded-table normal ">
+            <tbody>
+              <tr>
+                <td>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>Weight:</td>
+                        <td>35</td>
+                      </tr>
+                      <tr>
+                        <td>Height:</td>
+                        <td>3</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+                <td>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>Stat1:</td>
+                        <td>hp</td>
+                      </tr>
+                      <tr>
+                        <td>Stat2:</td>
+                        <td>attack</td>
+                      </tr>
+                      <tr>
+                        <td>Stat3:</td>
+                        <td>defense</td>
+                      </tr>
+                      <tr>
+                        <td>Stat4:</td>
+                        <td>special-attack</td>
+                      </tr>
+                      <tr>
+                        <td>Stat5:</td>
+                        <td>special-defense</td>
+                      </tr>
+                      <tr>
+                        <td>Stat6:</td>
+                        <td>speed</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+                <td>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>Bs1:</td>
+                        <td>30</td>
+                      </tr>
+                      <tr>
+                        <td>Bs2:</td>
+                        <td>56</td>
+                      </tr>
+                      <tr>
+                        <td>Bs3:</td>
+                        <td>35</td>
+                      </tr>
+                      <tr>
+                        <td>Bs4:</td>
+                        <td>25</td>
+                      </tr>
+                      <tr>
+                        <td>Bs5:</td>
+                        <td>35</td>
+                      </tr>
+                      <tr>
+                        <td>Bs6:</td>
+                        <td>72</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
 
