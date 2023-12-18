504749ffef02bdbfca02c61f01c0debd72921d6a
salman3009 <salman444g@gmail.com>
2023-12-18 21:27:54 +0530

    code

--- Course/December/18-12-23/pokemon-project/src/App.js
+++ Course/December/18-12-23/pokemon-project/src/App.js
@@ -9,6 +9,12 @@ function App() {
        let response = await fetch('https://content.newtonschool.co/v1/pr/64ccef982071a9ad01d36ff6/pokemonspages1');
        let data = await response.json();
        console.log(data[0].results);
+       let list = data[0].results;
+       for await(let obj of list){
+        let responsePokemon = await fetch(obj.url);
+        let dataPokemon = await responsePokemon.json();
+        console.log(dataPokemon);
+       }
      }catch(err){
       console.log(err);
      }
