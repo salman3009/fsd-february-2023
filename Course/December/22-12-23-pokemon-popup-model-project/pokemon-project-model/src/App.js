f2445f99f1cb72ec8eadca736b5683f4517e5dfd
salman3009 <salman444g@gmail.com>
2023-12-22 19:32:36 +0300

    code

--- Course/December/22-12-23-pokemon-popup-model-project/pokemon-project-model/src/App.js
+++ Course/December/22-12-23-pokemon-popup-model-project/pokemon-project-model/src/App.js
@@ -35,8 +35,8 @@ function App() {
     getAllPokemon();
   }
 
-  const modalEventHandler=(id)=>{
-    console.log(id);
+  const modalEventHandler=(event)=>{
+    console.log(event);
     setModal(!getModal);
   }
 
@@ -54,7 +54,7 @@ function App() {
                 <div className="detail-wrapper">
                   <h3>{obj.name.toUpperCase()}</h3>
                   <small>Type: {obj.type}</small>
-                  <button onClick={()=>modalEventHandler(index)} className="pokeinfo">Know more...</button>
+                  <button onClick={modalEventHandler} className="pokeinfo">Know more...</button>
                 </div>
               </div>)
             })}
