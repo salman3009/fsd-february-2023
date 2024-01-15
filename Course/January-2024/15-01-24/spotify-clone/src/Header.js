5ecf0c81b750fe62514dce5c1a71f1d5e97d68ca
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-01-15 21:23:51 +0530

    Code

--- Course/January-2024/15-01-24/spotify-clone/src/Header.js
+++ Course/January-2024/15-01-24/spotify-clone/src/Header.js
@@ -1,10 +1,36 @@
+import { Link } from 'react-router-dom';
 
+function Header() {
 
-function Header(){
-     
-    return (<>
-      <h1>Header component</h1>
-    </>)
+  return (<>
+    <nav>
+      <a href="/">
+        <img src="https://d3dyfaf3iutrxo.cloudfront.net/general/upload/78dbd1a43af24c48a5bc7ba17d4cb2dc.png" alt="spotify-logo" />
+      </a>
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+    <main id="download">
+      <h1>Introducing Premium Mini</h1>
+      <h2>₹7 for 1 day. ₹25 for 1 week.</h2>
+      <section>
+        <button>
+          <i class="fa-brands fa-google-play fa-xl"></i>
+          DOWNLOAD
+        </button>
+        <button>
+          <i class="fa-brands fa-apple fa-xl"></i>
+          DOWNLOAD
+        </button>
+      </section>
+    </main>
+  </>)
 
 }
 export default Header;
--- Course/January-2024/15-01-24/spotify-clone/src/Register.js
+++ Course/January-2024/15-01-24/spotify-clone/src/Register.js
@@ -6,7 +6,24 @@
 function Register(){
      
     return (<>
-      <h1>Register component</h1>
+     <section id="students-offer">
+    <h3>Special discount for eligible students in university</h3>
+    <button>LEARN MORE</button>
+    <form>
+        <input type="email" name="email" id="email" placeholder="Email address" required autocomplete="off" />
+        <input type="text" name="userName" id="userName" placeholder="User Name" required  autocomplete="off" />
+        <input type="password" name="password" id="password" placeholder="Password" required  autocomplete="off" />
+
+        <label for="appType">App Type</label>
+        <select name="appType" id="appType" required>
+            <option value="music">music</option>
+            <option value="album">album</option>
+        </select>
+        <input type="checkbox" name="consent-checkbox" id="consent-checkbox" required/>
+        <span>By checking this box and clicking "Submit", you consent to use information for verification.</span>
+        <button type="submit">SUBMIT</button>
+    </form>
+</section>
     </>)
 
 }
