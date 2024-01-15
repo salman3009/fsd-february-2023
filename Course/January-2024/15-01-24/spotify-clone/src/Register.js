683bb98965d6d31097add5ed769bfe3f0d469d18
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-01-15 22:36:17 +0530

    Code

--- Course/January-2024/15-01-24/spotify-clone/src/Register.js
+++ Course/January-2024/15-01-24/spotify-clone/src/Register.js
@@ -11,6 +11,8 @@ function Register() {
     appType: 'music'
   })
 
+  const[getError,setError] = useState("");
+
   const navigate = useNavigate();
 
 
@@ -20,11 +22,17 @@ function Register() {
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
+    setError('');
     axios.post("https://academics.newtonschool.co/api/v1/user/signup",getData).then((response)=>{
                console.log(response);
                navigate('/login');
     }).catch((error)=>{
-      console.log(error);
+      if(error.response && error.response.data && error.response.data.message){
+        setError(error.response.data.message);
+      }
+      else{
+        setError("unknow error please try after sometime");
+      }
     })
   }
 
@@ -34,6 +42,7 @@ function Register() {
       <h3>Please Register here</h3>
       <button>LEARN MORE</button>
       <form onSubmit={onSubmitHandler}>
+      <h2 style={{color:'red'}}>{getError}</h2>
         <input type="email" name="email" id="email" value={getData.email} onChange={onChangeHandler} placeholder="Email address" required autoComplete="off" />
         <input type="text" name="name" id="name" value={getData.userName} onChange={onChangeHandler} placeholder="User Name" required autoComplete="off" />
         <input type="password" name="password" id="password"  value={getData.password} onChange={onChangeHandler} placeholder="Password" required autoComplete="off" />
