ebed9333c7e231c1c3459e3d2da9b1f3eb0a6073
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-01-17 21:34:50 +0530

    Code

--- Course/January-2024/15-01-24/spotify-clone/src/Home.js
+++ Course/January-2024/15-01-24/spotify-clone/src/Home.js
@@ -1,8 +1,32 @@
+import axios from 'axios';
+import {useEffect,useState} from 'react';
 
 function Home(){
-     
+   
+  const[getList,setList]=useState([]);
+
+   const listOfDetails=async ()=>{
+           axios.get('https://academics.newtonschool.co/api/v1/music/song').then((response)=>{
+                       console.log(response.data.data);
+                      setList(response.data.data);
+           }).catch((error)=>{
+                       console.log(error);
+           })
+        
+   }
+    useEffect(()=>{
+      listOfDetails();
+    },[])
+
+
     return (<>
-      <h1>Home component</h1>
+       {
+        getList.map((obj,index)=>{
+                 return (<div key={index}>
+                  <img src={obj.thumbnail}/>
+                 </div>)
+        })
+       }
     </>)
 
 }
