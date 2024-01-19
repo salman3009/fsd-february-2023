2abd2f2bcabd67d6d970e5c433ccd0ebba3679c2
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-01-19 21:38:37 +0530

    Code

--- Course/January-2024/19-01-24/spotify-clone/src/UserProvider.js
+++ Course/January-2024/19-01-24/spotify-clone/src/UserProvider.js
@@ -1,7 +1,14 @@
-export const UserProvider = ({children})=>{
-    
-    return(<div>
-          {children} 
+import { createContext } from "react"
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+
+    return (<div>
+        <UserContext.Provider value={{ fullName: "akash", age: 33 }}>
+            {children}
+        </UserContext.Provider>
+
     </div>)
-   
+
 }
