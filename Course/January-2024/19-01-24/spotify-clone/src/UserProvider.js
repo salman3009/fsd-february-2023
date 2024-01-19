6cabafbcd8e914a514bf82ce6523de246c587e52
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-01-19 21:36:42 +0530

    Code

---
+++ Course/January-2024/19-01-24/spotify-clone/src/UserProvider.js
@@ -0,0 +1,7 @@
+export const UserProvider = ({children})=>{
+    
+    return(<div>
+          {children} 
+    </div>)
+   
+}
--- Course/January-2024/19-01-24/spotify-clone/src/index.js
+++ Course/January-2024/19-01-24/spotify-clone/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserProvider } from './UserProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <UserProvider>
     <App />
-  </React.StrictMode>
+  </UserProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
