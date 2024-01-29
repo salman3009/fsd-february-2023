7352953cd3b70346013a6aeace0fc3a9959ad773
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-01-29 21:55:13 +0530

    code

---
+++ Course/January-2024/29-01-24/react_ui_material/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import TextButtons from './TextButtons';
+function App() {
+  return (
+    <div className="App">
+       <TextButtons/>
+    </div>
+  );
+}
+
+export default App;
