843bd87ad892b643ac7d4de2301f6960b9ecfca5
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-02-05 22:59:53 +0530

    code

--- Course/Feb-2024/05-02-24/spotify-music-clone/src/App.js
+++ Course/Feb-2024/05-02-24/spotify-music-clone/src/App.js
@@ -8,7 +8,16 @@ function App() {
 
         </div>
         <div className="right-section">
-
+          <div className="nav-section">
+            <div>
+              <i className="fa-solid fa-angle-left nav-menu"></i>
+              <i className="fa-solid fa-angle-right nav-menu"></i>
+            </div>
+            <div>
+              <button className="nav-menu">Sign up</button>
+              <button className="nav-menu">Login</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
