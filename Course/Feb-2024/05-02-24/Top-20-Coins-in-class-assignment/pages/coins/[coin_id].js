b361db1a3c5834295f1cf202235567a252823247
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-02-05 21:49:06 +0530

    Code

--- Course/Feb-2024/05-02-24/Top-20-Coins-in-class-assignment/pages/coins/[coin_id].js
+++ Course/Feb-2024/05-02-24/Top-20-Coins-in-class-assignment/pages/coins/[coin_id].js
@@ -11,6 +11,7 @@ function CoinDetail() {
        const response = await fetch(`https://api.coinlore.net/api/ticker/?id=${coin_id}`);
        const result = await response.json();
        console.log(result);
+       setCoin(result[0])
      }catch(err){
       console.log(err);
      }
@@ -20,18 +21,22 @@ function CoinDetail() {
     fetchCoin();
   },[coin_id])
 
+  if(!coin){
+    return <p>Loading...</p>
+  }
+
   return (
     <div className="coin-detail">
       <div className="name-symbol">
-        <h1 className="name">coin-name</h1>
-        <h2 className="symbol">(coin-symbol)</h2>
+        <h1 className="name">{coin.name}</h1>
+        <h2 className="symbol">{coin.symbol}</h2>
       </div>
       <div className="market-description">
-        <p className="coin-rank">Rank: coin-symbol</p>
-        <p className="coin-price">Price: $coin-price</p>
-        <p className="coin-market-cap">Market Cap: $coin-market_cap_usd</p>
-        <p className="coin-total-supply">Total Supply: coin-tsupply</p>
-        <p className="coin-max-supply">Max Supply: coin-msupply</p>
+        <p className="coin-rank">Rank: {coin.rank}</p>
+        <p className="coin-price">Price: {coin.price_usd}</p>
+        <p className="coin-market-cap">Market Cap: {coin.market_cap_usd}</p>
+        <p className="coin-total-supply">Total Supply: {coin.tsupply}</p>
+        <p className="coin-max-supply">Max Supply: {coin.msupply || 'N/A'}</p>
       </div>
     </div>
   );
