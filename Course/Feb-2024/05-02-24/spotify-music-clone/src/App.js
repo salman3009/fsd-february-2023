a76f0f4c5d547857d28490d94d6647727d393f42
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-02-05 23:18:05 +0530

    code

--- Course/Feb-2024/05-02-24/spotify-music-clone/src/App.js
+++ Course/Feb-2024/05-02-24/spotify-music-clone/src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+axios.interceptors.request.use(async (config) => {
+  config.headers['projectid'] = "f104bi07c490";
+  return config;
+})
 
 function App() {
+
+  const [getList, setList] = useState([]);
+
+  const listOfDetails = async () => {
+    axios.get('https://academics.newtonschool.co/api/v1/music/song').then((response) => {
+      console.log(response.data.data);
+      setList(response.data.data);
+    }).catch((error) => {
+      console.log(error);
+    })
+
+  }
+  useEffect(() => {
+    listOfDetails();
+  }, [])
+
+
   return (
     <div>
       <div className="main-container">
         <div className="left-section">
-
         </div>
         <div className="right-section">
           <div className="nav-section">
@@ -16,6 +39,24 @@ function App() {
             <div>
               <button className="nav-menu">Sign up</button>
               <button className="nav-menu">Login</button>
+            </div>
+          </div>
+          <div className="sub-container">
+            <div className="sub-menu">
+              <div>Playlist</div>
+              <div>Show All</div>
+            </div>
+            <div className="list">
+              {
+                getList.map((obj, index) => {
+                  return (
+                    <div key={index} className="card-body">
+                      <img className="image" src={obj.thumbnail} />
+                      <div className="heading">{obj.title}</div>
+                    </div>
+                  )
+                })
+              }
             </div>
           </div>
         </div>
