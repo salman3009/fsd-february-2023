a632ee6de21d6546353026f8c7ae2b19e805b216
salman3009 <35424086+salman3009@users.noreply.github.com>
2024-02-05 22:47:53 +0530

    code

---
+++ Course/Feb-2024/05-02-24/spotify-music-clone/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="main-container">
+        <div className="left-section">
+
+        </div>
+        <div className="right-section">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
