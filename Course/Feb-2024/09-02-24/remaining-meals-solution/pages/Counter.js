aa820608093cf0421284556fb98213d60ac98bf0
salman3009 <salman444g@gmail.com>
2024-02-09 22:13:49 +0530

    code

--- Course/Feb-2024/09-02-24/remaining-meals-solution/pages/Counter.js
+++ Course/Feb-2024/09-02-24/remaining-meals-solution/pages/Counter.js
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+import { MealsContext } from "./MealsProvider";
+
 const Counter = () => {
+  const {meals} = useContext(MealsContext);
+  const result = meals.filter((obj)=>{
+     return !obj.ticked;
+  });
+
   return (
     <div>
-      <h3>Meals Remaining: remainingMeals</h3>
+      <h3>Meals Remaining: remainingMeals:{result.length}</h3>
     </div>
   );
 };
