e3a846e9fe41cd2499bc46d478d1241e5f636735
salman3009 <salman444g@gmail.com>
2024-02-12 21:48:43 +0530

    Code

--- Course/Feb-2024/12-02-24/react-stop-watch-assignment-solution/app/page.js
+++ Course/Feb-2024/12-02-24/react-stop-watch-assignment-solution/app/page.js
@@ -8,23 +8,55 @@ function Home() {
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
 
+  let secondsElapsed = 0;
+
+  const startStopWatch=()=>{
+   startTime.current = Date.now();
+   intervalRef.current = setInterval(()=>{
+      setCurrentTime(Date.now());
+   },10)
+  }
+
+  const stopWatch=()=>{
+     clearInterval(intervalRef.current)
+  }
+
+  const resetWatch=()=>{
+     setCurrentTime(0);
+     secondsElapsed = 0;
+     startTime.current=0;
+     clearInterval(intervalRef.current); 
+     setLaps([]);  
+  }
+
+  secondsElapsed = (currentTime - startTime.current)/1000;
+
+  const lapWatch=()=>{
+   setLaps([...laps,secondsElapsed]);
+  }
+
+
   return (
     <div id="main">
       <section>
-        <h1 className='seconds-elapsed'>Stopwatch Time</h1>
+        <h1 className='seconds-elapsed'>{secondsElapsed}</h1>
         <section className='buttons'>
-          <button className="start-btn">START</button>
-          <button className="stop-btn">STOP</button>
-          <button className="lap-btn">LAP</button>
-          <button className="reset-btn">RESET</button>
+          <button onClick={startStopWatch} className="start-btn">START</button>
+          <button onClick={stopWatch} className="stop-btn">STOP</button>
+          <button onClick={lapWatch} className="lap-btn">LAP</button>
+          <button onClick={resetWatch} className="reset-btn">RESET</button>
         </section>
       </section>
       <section className='lap-section'>
         <h2>Laps</h2>
         <section className='laps'>
-          <p>lap</p>
-          <p>lap</p>
-          <p>lap</p>
+        {
+        laps.map((obj)=>{
+          return (<p key={obj}>
+              {obj}
+          </p>)
+        })
+       }
         </section>
       </section>
     </div>
