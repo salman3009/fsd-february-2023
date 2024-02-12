34bf906a020b83d1ba0105650297f6941a20b812
salman3009 <salman444g@gmail.com>
2024-02-12 21:05:18 +0530

    code

---
+++ Course/Feb-2024/12-02-24/react-start-watch/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import {useRef, useState} from 'react';
+
+
+function App() {
+  
+   const startTime = useRef(0);
+   const intervalRef = useRef(0);
+   const [currenTime,setCurrentTime] = useState(0);
+
+   let secondsElapsed = 0;
+
+   const startStopWatch=()=>{
+    startTime.current = Date.now();
+    intervalRef.current = setInterval(()=>{
+      setCurrentTime(Date.now());
+    },10)
+   }
+
+   secondsElapsed = (currenTime - startTime.current)/1000;
+
+  return (
+    <div className="App">
+      <h1>{secondsElapsed.toFixed(3)}</h1>
+      <section>
+        <button onClick={startStopWatch}>Start</button>
+      </section>
+    </div>
+  );
+}
+
+export default App;
