35ac991ec1d42899621b534c74dbe03dbcc50b4c
salman3009 <salman444g@gmail.com>
2024-02-19 22:54:12 +0530

    Code

---
+++ Course/Feb-2024/19-02-24/food-list/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import FoodList from "./foodlist";
+function App() {
+  return (
+    <div className="App">
+       <FoodList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Course/Feb-2024/19-02-24/food-list/src/foodlist.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+// import "../styles/App.css";
+
+function FoodList() {
+	const [foods, setFoods] = useState([]);
+	const [itemName, setItemName] = useState("");
+	const [foodType, setFoodType] = useState("");
+	const [spicinessLevel, setSpicinessLevel] = useState("");
+	const [isFirstCardEnabled, setIsFirstCardEnabled] = useState(false);
+	const [isSecondCardEnabled, setIsSecondCardEnabled] = useState(false);
+	const [isFormEnabled, setIsFormEnabled] = useState(false);
+
+
+    const addFoodHandler=()=>{
+        setIsFirstCardEnabled(true);
+    }
+	return (
+		<>
+			<div className="container">
+				<h1>Food Items List</h1>
+				<button onClick={addFoodHandler}>Add Food</button>
+
+				<div className="card-container">
+                        {isFirstCardEnabled && <>
+							<h2>Item Name:</h2>
+							<input
+								name="itemName"
+								type="text"
+								disabled={!isFirstCardEnabled}
+							/>
+							<h2>Food Type:</h2>
+							<input
+								name="foodType"
+								type="text"
+								disabled={!isFirstCardEnabled}
+							/>
+							<div className={`card`}>
+								<form>
+									<h2>Spiciness Level:</h2>
+									<input
+										name="spicinessLevel"
+										type="text"
+										disabled={!isFormEnabled}
+									/>
+								</form>
+							</div>
+						</>}
+				</div>
+                <div className={`card ${isSecondCardEnabled ? "" : "disabled"}`}>
+						<button>Save</button>
+				</div>
+
+				<ul className="list">
+                        <li>
+							{itemName} ({foodType}) - Spiciness Level:{" "}
+							{spicinessLevel}
+							<button>Delete</button>
+						</li>
+				</ul>
+			</div>
+		</>
+	);
+}
+
+export default FoodList;
